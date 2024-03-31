095087774af1fc665a25d7d1c55e9e9d7155a9fe
Loginov Ilya <despair94frvr@mail.ru>
2024-03-31 22:04:37 +0300

    small doc fix

--- help.h
+++ help.h
@@ -8,12 +8,12 @@ void PrintInfo() {
                  "[-{filter 2} ...] ..."
               << std::endl;
     std::cout << "FILTERS:" << std::endl;
-    std::cout << "-crop width height : crops the image to the given width and height (top-left part is used)"
+    std::cout << "-crop {width} {height} : crops the image to the given width and height (top-left part is used)"
               << std::endl;
-    std::cout << "-gs                : converts the image to grayscale" << std::endl;
-    std::cout << "-neg               : converts the image to negative" << std::endl;
-    std::cout << "-sharp             : increases image sharpness" << std::endl;
-    std::cout << "-edge {threshold}  : detects image edges" << std::endl;
-    std::cout << "-spiral            : converts the image to spiral" << std::endl;
-    std::cout << "-blur {sigma}      : blurs the image" << std::endl;
+    std::cout << "-gs                    : converts the image to grayscale" << std::endl;
+    std::cout << "-neg                   : converts the image to negative" << std::endl;
+    std::cout << "-sharp                 : increases image sharpness" << std::endl;
+    std::cout << "-edge {threshold}      : detects image edges" << std::endl;
+    std::cout << "-spiral                : converts the image to spiral" << std::endl;
+    std::cout << "-blur {sigma}          : blurs the image" << std::endl;
 }
